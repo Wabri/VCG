7b623a0f6a32adce0dba1b538020192bbee08cf9
Wabri <gabriele.puliti@gmail.com>
2018-04-22 17:13:21 +0200

    update main with some refactoring

--- VCG/src/main/cpp/main.cpp
+++ VCG/src/main/cpp/main.cpp
@@ -5,111 +5,86 @@
 
 using namespace std;
 
+void stringRequest(string* hereTheResult, string firstInput,
+		string inputConfirm) {
+	do {
+		cout << "### " + firstInput + " ###" << endl;
+		cout << " Input: ";
+		cin >> *hereTheResult;
+		cout << "### You have insert: " << *hereTheResult << " ###" << endl
+				<< "### " + inputConfirm + " (Y: yes, N: no) ###" << endl;
+		cout << " Input: ";
+		string response;
+		cin >> response;
+		while (!((response == "Y") || (response == "y") || (response == "N")
+				|| (response == "n"))) {
+			cout << "### You have to insert Y or N ###" << endl;
+			cout << " Input: ";
+			cin >> response;
+		}
+		if ((response == "Y") || (response == "y")) {
+			cout << "### Confirmed ###" << endl << endl;
+			response.~basic_string();
+			break;
+		} else if ((response == "N") || (response == "n")) {
+			cout << "### Not confirmed ###" << endl << endl;
+		}
+	} while (true);
+}
+
+int extractAlphabethFromString(string hereTheResult[], string *extractThis) {
+	int *index = new int;
+	int counter = 0;
+	while (true) {
+		int positionComma = extractThis->find(',');
+		string newElement = extractThis->substr(0, positionComma);
+		hereTheResult[(*index)++] += newElement;
+		newElement = extractThis->substr(1 + positionComma,
+				extractThis->length());
+		*extractThis = newElement;
+		counter++;
+		if (positionComma == string::npos) {
+			delete index;
+			return counter;
+		}
+	}
+}
+
 int main(int argc, char **argv) {
 	if (argc == 1) {
-		cout << "### Missing an alphabeth to work with ###" << endl;
+		cout << endl << "### Alphabeth ###" << endl;
 		string alphabethString;
-		do {
-			cout
-					<< "### Insert the elements of alphabeth separated with comma (example: 1,2,3,A,B) ###"
-					<< endl;
-			cout << " Input: ";
-			cin >> alphabethString;
-			cout << "### You have insert: " << alphabethString << " ###" << endl
-					<< "### Confirm this alphabeth? (Y: yes, N: no) ###"
-					<< endl;
-			cout << " Input: ";
-			string response;
-			cin >> response;
-			while (!((response == "Y") || (response == "y") || (response == "N")
-					|| (response == "n"))) {
-				cout << "### You have to insert Y or N ###" << endl;
-				cout << " Input: ";
-				cin >> response;
-			}
-			if ((response == "Y") || (response == "y")) {
-				cout << "### Alphabeth confirmed ###" << endl;
-				response.~basic_string();
-				break;
-			} else if ((response == "N") || (response == "n")) {
-				cout << "### Alphabeth not confirmed ###" << endl;
-			}
-		} while (true);
-		string alphabethArray[alphabethString.size()];
-		int *index = new int;
-		while (true) {
-			int positionComma = alphabethString.find(',');
-			string newElement = alphabethString.substr(0, positionComma);
-			alphabethArray[(*index)++] += newElement;
-			newElement = alphabethString.substr(1 + positionComma,
-					alphabethString.length());
-			alphabethString = newElement;
-			if (positionComma == string::npos) {
-				break;
-			}
-		}
+		stringRequest(&alphabethString,
+				"Insert the elements of alphabeth separated with comma (example: 1,2,3,A,B)",
+				"Confirm this alphabeth?");
+		string *temp = new string[alphabethString.size()];
+		int numberOfElementsInAlphabeth = 0;
+		numberOfElementsInAlphabeth = extractAlphabethFromString(temp,
+				&alphabethString);
 		alphabethString.~basic_string();
-		delete index;
+		string alphabethArray[numberOfElementsInAlphabeth];
+		for (int index = 0; index < numberOfElementsInAlphabeth; index++) {
+			alphabethArray[index] = temp[index];
+		}
+		delete[] temp;
+
+		cout << "### Number of characters ###" << endl;
 		string characters;
-		do {
-			cout
-					<< "### How many characters must have a single element of output string? (example: 2 characters 1A, 3 characters B3F)###"
-					<< endl;
-			cout << " Input: ";
-			cin >> characters;
-			cout << "### You have insert: " << characters << " ###" << endl
-					<< "### Confirm this number of characters? (Y: yes, N: no) ###"
-					<< endl;
-			cout << " Input: ";
-			string response;
-			cin >> response;
-			while (!((response == "Y") || (response == "y") || (response == "N")
-					|| (response == "n"))) {
-				cout << "### You have to insert Y or N ###" << endl;
-				cout << " Input: ";
-				cin >> response;
-			}
-			if ((response == "Y") || (response == "y")) {
-				cout << "### Number of characters confirmed ###" << endl;
-				response.~basic_string();
-				break;
-			} else if ((response == "N") || (response == "n")) {
-				cout << "### Number of characters not confirmed ###" << endl;
-			}
-		} while (true);
+		stringRequest(&characters,
+				"How many characters must have a single element of output string? (example: 2 characters 1A, 3 characters B3F)",
+				"Confirm this number of characters?");
 		int numberOfCharacters = atoi(characters.c_str());
 		characters.~basic_string();
+
+		cout << "### Number of elements ###" << endl;
 		string elements;
-		do {
-			cout
-					<< "### How many output elements do you want? (example: 3 elements in output -> AB CD 35, 4 elements in output -> AB CD 35 13)###"
-					<< endl;
-			cout << " Input: ";
-			cin >> elements;
-			cout << "### You have insert: " << elements << " ###" << endl
-					<< "### Confirm this number of elements in output? (Y: yes, N: no) ###"
-					<< endl;
-			cout << " Input: ";
-			string response;
-			cin >> response;
-			while (!((response == "Y") || (response == "y") || (response == "N")
-					|| (response == "n"))) {
-				cout << "### You have to insert Y or N ###" << endl;
-				cout << " Input: ";
-				cin >> response;
-			}
-			if ((response == "Y") || (response == "y")) {
-				cout << "### Number of elements in output confirmed ###"
-						<< endl;
-				response.~basic_string();
-				break;
-			} else if ((response == "N") || (response == "n")) {
-				cout << "### Number of elements in output not confirmed ###"
-						<< endl;
-			}
-		} while (true);
+		stringRequest(&elements,
+				"How many output elements do you want? (example: 3 elements in output -> AB CD 35, 4 elements in output -> AB CD 35 13)",
+				"Confirm this number of elements in output?");
 		int numberOfElements = atoi(elements.c_str());
 		elements.~basic_string();
+
+		srand(time(0));
 		string output;
 		for (int var = 0; var < numberOfElements; ++var) {
 			for (int rip = 0; rip < numberOfCharacters; ++rip) {
